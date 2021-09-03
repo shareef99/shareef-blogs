46f975b3d5709e087c15fdfcc3b03f34d458f332
shareef <nadeemshareef934@gmail.com>
2021-09-03 20:53:26 +0530

    🎉 finish writing extension

--- extension.js
+++ extension.js
@@ -1,36 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const { default: axios } = require("axios");
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+    console.log(
+        'Congratulations, your extension "shareef-blogs" is now active!'
+    );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "shareef-blogs" is now active!');
+    const res = await axios.get("https://dev.to/api/articles?username=shareef");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('shareef-blogs.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+    const articles = res.data.map((article) => {
+        return {
+            label: article.title,
+            detail: article.description,
+            link: article.url,
+        };
+    });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from shareef-blogs!');
-	});
+    let disposable = vscode.commands.registerCommand(
+        "shareef-blogs.searchShareefBlogs",
 
-	context.subscriptions.push(disposable);
+        async function () {
+            const article = await vscode.window.showQuickPick(articles, {
+                matchOnDetail: true,
+            });
+
+            if (article == null) return;
+
+            vscode.env.openExternal(article.link);
+
+            vscode.window.showInformationMessage(
+                "Hello World from shareef-blogs!"
+            );
+        }
+    );
+
+    context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate,
+};
